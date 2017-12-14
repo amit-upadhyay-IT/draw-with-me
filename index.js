4c47a37fa30e365d996ffbc6fad66f2e5a7f9437
amit-upadhyay-it <youarefreak1@gmail.com>
2017-12-14 18:18:09 +0530

    done success

--- index.js
+++ index.js
@@ -6,20 +6,18 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-/*
 io.on('connection', function(socket) {
     console.log('a user connected');
 
-    socket.on('chat message', function(msg) {
-        console.log('chat message', msg);
-        socket.broadcast.emit('send message', msg);
+    socket.on('draw', function(msg) {
+        console.log('drawing', msg);
+        socket.broadcast.emit('draw', msg);
     });
 
     socket.on('disconnect', function () {
         console.log('a user disconnected');
     });
 });
-*/
 
 http.listen(3000, function () {
     console.log('listening on *:3000');
