07f98b984cbc68cde26d289e99eac62ea3e57738
amit-upadhyay-it <youarefreak1@gmail.com>
2017-12-14 18:53:45 +0530

    process.env.port used

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var http = require('http').Server(app);
 
 var io = require('socket.io')(http);
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -22,7 +24,7 @@ io.on('connection', function(socket) {
 
 });
 
-http.listen(3000, function () {
+http.listen(port, function () {
     console.log('listening on *:3000');
 });
 
