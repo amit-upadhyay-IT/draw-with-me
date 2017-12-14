87ec7ee8ae4f9c83b72d6f55f25d274748a72a90
amit-upadhyay-it <youarefreak1@gmail.com>
2017-12-14 18:27:29 +0530

    test

---
+++ app/test.js
@@ -0,0 +1,64 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('a user connected');
+
+    socket.on('draw', function(msg) {
+        console.log('drawing', msg);
+        socket.broadcast.emit('draw', msg);
+    });
+
+    socket.on('disconnect', function () {
+        console.log('a user disconnected');
+    });
+});
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
